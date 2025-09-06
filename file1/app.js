83cfa7ac1c0d8f26bb61676225a54421084e61db
MUKUVI <mukuvi@localhost.localdomain>
2025-09-06 23:14:22 +0300

    horizon

--- file1/app.js
+++ file1/app.js
@@ -3,12 +3,13 @@ let hText = document.getElementById("h-text");
 const hBtn = document.getElementById("h-btn");
 
 let count = 0;
-let firstName = "james mukuvi";
-let lastName = "Ngandu";
-let fullName = firstName + " " + lastName;
-
-hText.innerText = fullName;
-
+if (count <= 0) {
+  hText.innerText = "count is less than 0";
+} else if ((count = 5)) {
+  hText.innerText = "count is 0";
+} else {
+  hText.innerText = "count is greater than 0";
+}
 hBtn.addEventListener("click", () => {
   count++;
   hValue.innerText = Math.floor(Math.random() * 9);
