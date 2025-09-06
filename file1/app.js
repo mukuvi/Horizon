6be9196e44d8c944e0556287fb9aa4811478ba58
MUKUVI <mukuvi@localhost.localdomain>
2025-09-06 23:06:28 +0300

    horizon

--- file1/app.js
+++ file1/app.js
@@ -1,7 +1,15 @@
 const hValue = document.getElementById("h-val");
+let hText = document.getElementById("h-text");
 const hBtn = document.getElementById("h-btn");
+
 let count = 0;
+let firstName = "james mukuvi";
+let lastName = "Ngandu";
+let fullName = firstName + " " + lastName;
+
+hText.innerText = fullName;
 
 hBtn.addEventListener("click", () => {
-  hValue.textContent = count++;
+  count++;
+  hValue.innerText = Math.floor(Math.random() * 9);
 });
