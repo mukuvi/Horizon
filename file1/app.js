739a17c67c11429578fc09d3777ea3d9f2e77978
MUKUVI <mukuvi@localhost.localdomain>
2025-09-09 23:27:34 +0300

    Horizon

--- file1/app.js
+++ file1/app.js
@@ -5,23 +5,3 @@ function carsData() {
   return cars[randomCar];
 }
 console.log(carsData());
-
-let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"];
-let appleShelf = document.getElementById("apple-shelf");
-let orangeShelf = document.getElementById("orange-shelf");
-
-// Create a function that puts the apples onto the appleShelf
-// and the oranges onto the orangeShelf. Use a for loop,
-// a conditional statement, and the textContent property.
-
-function sortFruit() {
-  for (let i = 0; i < fruit.length; i++) {
-    if (fruit[i] === "🍎") {
-      appleShelf.textContent += "🍎";
-    } else if (fruit[i] === "🍊") {
-      orangeShelf.textContent += "🍊";
-    }
-  }
-}
-
-sortFruit();
