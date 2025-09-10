466e084d8acfca500e1277cd4e89229c361615a6
MUKUVI <mukuvi@localhost.localdomain>
2025-09-10 23:11:08 +0300

    horizon

--- file1/app.js
+++ file1/app.js
@@ -1,18 +1,3 @@
-let cars = ["benz", "audi", "porsche"];
-
-function carsData() {
-  let randomCar = Math.floor(Math.random() * 3);
-  return cars[randomCar];
-}
-// console.log(carsData());
-
-function logCars(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
-}
-// logCars(cars);
-
 let inputVal = document.querySelector("#h-val");
 let btnVal = document.querySelector("#h-btn");
 let storageVal = document.querySelector("#h-text");
@@ -23,6 +8,6 @@ btnVal.addEventListener("click", () => {
   storangeData.push(inputVal.value);
   storageVal.textContent = storangeData.join(", ");
   inputVal.value = "";
-  localStorage.setItem("storangeData", JSON.stringify(storangeData));
-  localStorage.getItem("storangeData");
+  // localStorage.setItem("storangeData", JSON.stringify(storangeData));
+  // localStorage.getItem("storangeData");
 });
