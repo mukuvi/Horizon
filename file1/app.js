1619daadfbcc46ad4a203f35974d3827d9c075ea
MUKUVI <mukuvi@localhost.localdomain>
2025-09-10 10:19:57 +0300

    Horizon

--- file1/app.js
+++ file1/app.js
@@ -4,4 +4,11 @@ function carsData() {
   let randomCar = Math.floor(Math.random() * 3);
   return cars[randomCar];
 }
-console.log(carsData());
+// console.log(carsData());
+
+function logCars(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+logCars(cars);
