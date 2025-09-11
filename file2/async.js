bc627596609d15232a25fa98a3988febad235696
MUKUVI <mukuvi@localhost.localdomain>
2025-09-11 22:27:06 +0300

    Horizon

--- file2/async.js
+++ file2/async.js
@@ -1,7 +1,17 @@
-setTimeout(() => {
-  console.log("My name is James Mukuvi Ngandu");
-}, 2000);
+// setTimeout(() => {
+//   console.log("My name is James Mukuvi Ngandu");
+// }, 2000);
 
-setInterval(() => {
-  console.log("call me James Mukuvi Ngandu");
-}, 2000);
+// setInterval(() => {
+//   console.log("call me James Mukuvi Ngandu");
+// }, 2000);
+
+function ins1() {
+  console.log("instruction one is being executed");
+}
+function ins2() {
+  console.log("instruction one is being executed");
+}
+function ins3() {
+  console.log("instruction one is being executed");
+}
