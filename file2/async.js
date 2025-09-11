919778514a8798a1814bfd9cc35cbaf3c52c571b
MUKUVI <mukuvi@localhost.localdomain>
2025-09-11 22:55:06 +0300

    Horizon

--- file2/async.js
+++ file2/async.js
@@ -6,8 +6,16 @@
 //   console.log("call me James Mukuvi Ngandu");
 // }, 2000);
 
-let isopen = true;
+let isopen = false;
 
-let open = () => {
-  console.log("hello");
+// let open = () => {
+//   isopen
+//     ? console.log("switch on the lights")
+//     : console.log("switch off the lights");
+// };
+// open();
+let open = (time, work) => {
+  return new Promise((resolve, reject) => {
+    isopen ? resolve(work()) : reject(console.log("our shop is closed"));
+  });
 };
