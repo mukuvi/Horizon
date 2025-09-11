8b5f0782afa1d949751f4700e189d0fa0c8b0bf0
MUKUVI <mukuvi@localhost.localdomain>
2025-09-11 22:29:07 +0300

    Horizon

--- file2/async.js
+++ file2/async.js
@@ -8,10 +8,14 @@
 
 function ins1() {
   console.log("instruction one is being executed");
+
+  ins2();
 }
 function ins2() {
-  console.log("instruction one is being executed");
+  console.log("instruction two is being executed");
 }
 function ins3() {
-  console.log("instruction one is being executed");
+  console.log("instruction three is being executed");
 }
+ins3();
+ins1();
