5d72541f9aacafb4e4594a2086f330de306ab245
MUKUVI <mukuvi@localhost.localdomain>
2025-09-11 21:54:06 +0300

    Horizon

--- file2/async.js
+++ file2/async.js
@@ -1,6 +1,7 @@
 setTimeout(() => {
   console.log("My name is James Mukuvi Ngandu");
 }, 2000);
+
 setInterval(() => {
-  console.log("My name is James Mukuvi Ngandu");
+  console.log("call me James Mukuvi Ngandu");
 }, 2000);
