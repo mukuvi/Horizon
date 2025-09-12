6c802ea076e8da184ada9eb387d0663b7a74c8a7
MUKUVI <mukuvi@localhost.localdomain>
2025-09-13 00:03:04 +0300

    Horizon

--- file2/async.js
+++ file2/async.js
@@ -42,10 +42,6 @@
 // }
 // product();
 
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-
 fetch("https://jsonplaceholder.typicode.com/posts/1")
   .then((response) => response.json())
   .then((data) => console.log(data));
