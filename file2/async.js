c8a510d6a98ea094a56a298f1c2ee141794298a1
MUKUVI <mukuvi@localhost.localdomain>
2025-09-12 13:33:25 +0300

    Horizon

--- file2/async.js
+++ file2/async.js
@@ -45,3 +45,7 @@
 fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
