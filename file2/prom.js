c47f6e04a1ac823d062e351bd8f63be1c9369f90
MUKUVI <mukuvi@localhost.localdomain>
2025-09-13 00:13:39 +0300

    Horizon

--- file2/prom.js
+++ file2/prom.js
@@ -1,10 +1,10 @@
 let a = new Promise((resolve, reject) => {
-  let b = 6 + 1;
+  let b = 1 + 1;
   if (b === 2) {
     resolve("success");
   } else {
     reject("Failed");
   }
 })
-  .then(() => console.log("hello"))
-  .catch((error) => console.log(error));
+  .then((message) => console.log(`The display shows ${message}`))
+  .catch((message) => console.log(`The display shows ${message}`));
