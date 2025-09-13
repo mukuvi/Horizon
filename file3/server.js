b9da92379363efeb748f1370da1a27eaa6dfc4a5
MUKUVI <mukuvi@localhost.localdomain>
2025-09-13 10:50:43 +0300

    horiozons

--- file3/server.js
+++ file3/server.js
@@ -1,7 +1,8 @@
 import http from "node:http";
 const PORT = 8000;
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
+  res.write("Hello from the server \n");
+  res.end();
 });
 
 server.listen(PORT, () => console.log(`server running on port: ${PORT}`));
