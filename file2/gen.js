af583d4c94a2e87746aa57875d70aa6ac857ff11
MUKUVI <mukuvi@localhost.localdomain>
2025-09-13 09:34:47 +0300

    Horizon

--- file2/gen.js
+++ file2/gen.js
@@ -1,38 +1,24 @@
-// let stocks = {
-//   fruits: ["mango", "banana", "pineapple", "orange"],
-//   toppings: ["chocolates", "peanuts"],
-//   drinks: ["water", "milk"],
-// };
-// let isopen = false;
+let stocks = {
+  fruits: ["mango", "banana", "pineapple", "orange"],
+  toppings: ["chocolates", "peanuts"],
+  drinks: ["water", "milk"],
+};
+let isopen = false;
 
-// let order = (time, work) => {
-//   return new Promise((resolve, reject) => {
-//     if (isopen) {
-//       setTimeout(() => {
-//         resolve(work());
-//       }, time);
-//     } else {
-//       reject("Our shop is closed");
-//     }
-//   });
-// };
-// order(2000, () => console.log(`${stocks.fruits[0]} was selected`)).catch(
-//   (err) => console.log(err)
-// );
-
-// let grade = 3;
-// async function product() {
-//   try {
-//     if (grade > 70) {
-//       console.log("Proceed to the next class");
-//     }
-//   } catch (error) {
-//     console.log("you cant proceed to the next class", error);
-//   } finally {
-//     console.log("Grading done!");
-//   }
-// }
-// product();
+let order = (time, work) => {
+  return new Promise((resolve, reject) => {
+    if (isopen) {
+      setTimeout(() => {
+        resolve(work());
+      }, time);
+    } else {
+      reject("Our shop is closed");
+    }
+  });
+};
+order(2000, () => console.log(`${stocks.fruits[0]} was selected`)).catch(
+  (err) => console.log(err)
+);
 
 fetch("https://jsonplaceholder.typicode.com/posts/1")
   .then((response) => response.json())
