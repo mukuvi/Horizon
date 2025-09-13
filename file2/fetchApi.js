ae6081c085b0e683cc644c3c9221a9ac5c2699bb
MUKUVI <mukuvi@localhost.localdomain>
2025-09-13 09:31:34 +0300

    Horizon

--- file2/fetchApi.js
+++ file2/fetchApi.js
@@ -1,11 +1,11 @@
 async function userData() {
   try {
-    let response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    let response = await fetch("https://jsonplaceholder.typicode.com/posts/p1");
     if (response.ok) {
       let data = await response.json();
       console.log(`User data: ${data.title}`);
     } else {
-      throw new Error();
+      throw new Error(`Request failed with status ${response.status} `);
     }
   } catch (error) {
     console.log(`An error occured ${error}`);
