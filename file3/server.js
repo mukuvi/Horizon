1115b0964ecc16f888e13c027ae6c9b2cac7bdc2
MUKUVI <mukuvi@localhost.localdomain>
2025-09-13 13:25:10 +0300

    horiozons

--- file3/server.js
+++ file3/server.js
@@ -1,5 +1,8 @@
 import http from "node:http";
+import { getDataFromDB } from "../db.js";
+
 const PORT = 8000;
+
 const server = http.createServer((req, res) => {
   if (req.url === "/api" && req.method === "GET") {
     res.end("This is from the server");
