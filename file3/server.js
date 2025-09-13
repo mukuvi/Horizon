d4c66156fdde6971fb0643ea78aa9be7eb34d667
MUKUVI <mukuvi@localhost.localdomain>
2025-09-13 15:28:47 +0300

    horiozons

--- file3/server.js
+++ file3/server.js
@@ -7,9 +7,20 @@ const PORT = 8000;
 const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB();
 
-  //   if (req.url === "/api" && req.method === "GET") {
-  res.end(JSON.stringify(destinations));
-  //   }
+  if (req.url === "/" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(destinations));
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 404;
+    res.end(
+      JSON.stringify({
+        error: "not found",
+        message: "The requested route does not exist",
+      })
+    );
+  }
 });
 
 server.listen(PORT, () => console.log(`server running on port: ${PORT}`));
