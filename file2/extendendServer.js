28a9dffb4c3cd084f177f78abf262904eca8333d
MUKUVI <mukuvi@localhost.localdomain>
2025-09-15 20:31:53 +0300

    Horizon

--- file2/extendendServer.js
+++ file2/extendendServer.js
@@ -3,6 +3,8 @@ const PORT = 8888;
 
 let server = http.createServer((req, res) => {
   if (req.url === "/api" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
   }
   res.end("am here");
 });
