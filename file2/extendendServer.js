9f1253b3b240b5259348f070350eb7ef1b140e6d
MUKUVI <mukuvi@localhost.localdomain>
2025-09-15 19:16:07 +0300

    Horizon

--- file2/extendendServer.js
+++ file2/extendendServer.js
@@ -1,2 +1,8 @@
 import http from "node:http";
 const PORT = 8888;
+
+let server = http.createServer((req, res) => {
+  res.end("am here");
+});
+
+server.listen(PORT, console.log(`server running under port ${PORT}`));
