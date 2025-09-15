0fe9bff58ae2c8dc674d3fe9fc19bca4f07d2f5e
MUKUVI <mukuvi@localhost.localdomain>
2025-09-15 21:44:29 +0300

    Horizon

--- file2/server.js
+++ file2/server.js
@@ -1,5 +1,6 @@
 import http from "node:http";
 import { getDataFromDB } from "./database/db.js";
+import { sendJSONResponse } from "./utils/sendJSONResponse.js";
 
 const PORT = 8000;
 
@@ -7,9 +8,7 @@ const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB();
 
   if (req.url === "/api" && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode = 200;
-    res.end(JSON.stringify(destinations));
+    sendJSONResponse(res, 200, destinations);
   } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
     const continent = req.url.split("/").pop();
     const filteredData = destinations.filter((destination) => {
