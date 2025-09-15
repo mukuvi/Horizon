ef0d28582152c834f7f95e07fb8ed5522e04e021
MUKUVI <mukuvi@localhost.localdomain>
2025-09-15 22:01:41 +0300

    Horizon

--- file2/server.js
+++ file2/server.js
@@ -15,6 +15,12 @@ const server = http.createServer(async (req, res) => {
       return destination.continent.toLowerCase() === continent.toLowerCase();
     });
     sendJSONResponse(res, 200, filteredData);
+  } else if (req.url.startsWith("/api/country") && req.method === "GET") {
+    const country = req.url.split("/").pop();
+    const filteredData = destinations.filter((destination) => {
+      return destination.country.toLowerCase() === country.toLowerCase();
+    });
+    sendJSONResponse(res, 200, filteredData);
   } else {
     sendJSONResponse(res, 404, {
       error: "not found",
