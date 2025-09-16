42f5004dce1fae4334407f43305a0bb47e7dec98
MUKUVI <mukuvi@localhost.localdomain>
2025-09-16 22:54:30 +0300

    Horizon

--- file3/server.js
+++ file3/server.js
@@ -2,7 +2,11 @@ import http from "node:http";
 const PORT = 8000;
 
 let server = http.createServer((req, res) => {
-  res.end("This is my server");
+  if (req.url === "/api") {
+    req.end("My server is correctly running");
+  } else {
+    res.end("Server error");
+  }
 });
 
 server.listen(PORT, console.log(`sever running on port ${PORT}`));
