3afbe3368b8fc2f6edd2775775d77f9fd8cd581f
MUKUVI <mukuvi@localhost.localdomain>
2025-09-16 19:22:45 +0300

    Horizon

--- file2/utils/sendJSONResponse.js
+++ file2/utils/sendJSONResponse.js
@@ -1,5 +1,7 @@
-export const sendJSONResponse = (res, statusCode, payLoad) => {
+export const sendJSONResponse = (res, statusCode, payload) => {
   res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
   res.statusCode = statusCode;
-  res.end(JSON.stringify(payLoad));
+  res.end(JSON.stringify(payload));
 };
