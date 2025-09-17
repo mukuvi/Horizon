4ac86891fa83d0f43aa0703a712e163574fbf1e3
MUKUVI <mukuvi@localhost.localdomain>
2025-09-17 23:17:44 +0300

    Horizon

--- file3/server.js
+++ file3/server.js
@@ -6,5 +6,12 @@ const PORT = 8000;
 const app = express();
 
 app.use("/api", apiRouter);
+app.use((req, res) => {
+  res
+    .status(404)
+    .json({
+      message: "Endpoint not found. Please check the API documentation.",
+    });
+});
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
