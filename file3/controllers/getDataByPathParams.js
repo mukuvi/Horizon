03193cc6f0d84f8388ee2af15132a25b5c6ecac6
MUKUVI <mukuvi@localhost.localdomain>
2025-09-17 21:48:08 +0300

    Horizon

--- file3/controllers/getDataByPathParams.js
+++ file3/controllers/getDataByPathParams.js
@@ -2,6 +2,17 @@ import { startups } from "../data/data";
 export const getDataByPathParams = (req, res) => {
   const { field, term } = req.params;
 
+  const allowedFields = ["country", "continent", "industry"];
+
+  if (!allowedFields.includes(field)) {
+    return res
+      .status(400)
+      .json({
+        message:
+          "Search field not allowed. Please use only 'country', 'continent', 'industry'",
+      });
+  }
+
   const filteredData = startups.filter(
     (startup) => startup[field].toLowerCase() === term.toLowerCase()
   );
