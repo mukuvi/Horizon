ea34462140290a4bea6734390e483df2d6461ec4
MUKUVI <mukuvi@localhost.localdomain>
2025-09-17 14:58:17 +0300

    Horizon

--- file3/server.js
+++ file3/server.js
@@ -4,4 +4,14 @@ const PORT = 8000;
 
 const app = express();
 
+const person = {
+  name: "james",
+  age: 22,
+  hobbies: ["swimming", "cooking", "coding"],
+};
+
+app.get("/", (req, res) => {
+  res.json(person);
+});
+
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
