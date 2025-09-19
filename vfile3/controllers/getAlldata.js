5fb0976f8fcca3187c7b30276e4bed3e9ccddeef
MUKUVI <mukuvi@localhost.localdomain>
2025-09-19 13:44:19 +0300

    Horizon

---
+++ vfile3/controllers/getAlldata.js
@@ -0,0 +1,22 @@
+import counties from "../data/data";
+app.get("/api", (req, res) => {
+  let filteredData = counties;
+  let { name, province, capital } = req.query;
+  if (name) {
+    filteredData = filteredData.filter(
+      (county) => county.name.toLowerCase() === name.toLowerCase()
+    );
+  }
+  if (province) {
+    filteredData = filteredData.filter(
+      (county) => county.province.toLowerCase() === province.toLowerCase()
+    );
+  }
+  if (capital) {
+    filteredData = filteredData.filter(
+      (county) => county.capital.toLowerCase() === capital.toLowerCase()
+    );
+  }
+
+  res.json(filteredData);
+});
