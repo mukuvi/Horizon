d2d1b5fcfea66ffc44b43486fb73b883e16691ab
mukuvi <jamesmngandu@gmail.com>
2025-09-23 16:44:07 +0300

    Horizon

---
+++ xfile/tadabase.js
@@ -0,0 +1,28 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import path from "node:path";
+
+async function createTable() {
+  const db = await open({
+    filename: path.join("tadabase.db"),
+    driver: sqlite3.Database,
+  });
+  await db.exec(`
+        CREATE TABLE IF NOT EXISTS cafesynca(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        country TEXT,
+        price REAL
+        )
+        `);
+  await db.run(`
+            INSERT INTO cafesynca(name,country,price)
+            VALLUES
+            ("arabica","ethiopia",760),
+            ("robuster,"kenya",500),
+            ("colombian","colombia"990)
+            `);
+  await db.close();
+  console.log("Database created");
+}
+createTable();
