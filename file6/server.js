7401a20a6843f2a558a2ecaf4f544c43292f1988
mukuvi <jamesmngandu@gmail.com>
2025-10-05 00:09:27 +0300

    Horizon

--- file6/server.js
+++ file6/server.js
@@ -3,7 +3,7 @@ import connectDB from "./db/connectDB.js";
 const app = express();
 const PORT = process.env.PORT || 8000;
 const DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/person";
+  process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/movies";
 connectDB(DATABASE_URL);
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
