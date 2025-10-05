0334164c558169758234a2271d81c480cbf66a87
mukuvi <jamesmngandu@gmail.com>
2025-10-05 14:34:51 +0300

    Horizon

--- file6/db/connectDB.js
+++ file6/db/connectDB.js
@@ -9,3 +9,11 @@ const connectDB = async (DATABASE_URL) => {
   }
 };
 export default connectDB;
+
+const deleteBD = async (DB_URL) => {
+  try {
+    await mongoose.connect(DB_URL);
+  } catch (error) {
+    console.log(error);
+  }
+};
